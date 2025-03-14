c9c87391b09c67591164265fb910570f183056f2
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-03-14 09:35:12 -0400

    Implementing Add Heading functionality

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -6,6 +6,13 @@ const TodoList = () => {
   const [headingInput, setHeadingInput] = useState('');
   const [listInputs, setListInputs] = useState({});
 
+  const handleAddTodo = () => {
+    if(headingInput.trim() !== '') {
+        setTodos([...todos, {heading : headingInput, lists: []}]);
+        setHeadingInput('');
+    }
+  }
+
   return (
     <>
       <div className="todo-container">
@@ -15,13 +22,22 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value)}}
             
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
-        
+        {todos.map((todo, index) => (
+            <div key={index} className="todo-card">
+                <div className="heading_todo">
+                    <h3> {todo.heading} </h3>
+                    <button className='delete-button-heading' onClick={() => handleDeleteTodo(index)}>Delete Heading</button>
+                </div>
+            </div>
+        ))}
       </div>
     </>
   );
